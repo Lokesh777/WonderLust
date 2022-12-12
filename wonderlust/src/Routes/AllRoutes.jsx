0298fbb2a51c90a21dd3ed950e6cf7bc27b7bac9
Lokesh777 <lokeshdevgan777@gmail.com>
2022-12-12 22:25:20 +0530

    updates

---
+++ wonderlust/src/Routes/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import { Routes } from "react-router-dom";
+
+ const Allroutes=()=>{
+
+    return (
+        <div>
+           <Routes>
+                <Route path=""  ></Route>
+
+           </Routes>
+        
+        </div>
+    )
+}
+export default Allroutes;
