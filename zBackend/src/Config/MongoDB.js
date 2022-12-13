152346897b64f8c73cba85908b991eeda4d3b23d
Lv-23 <lokeshvyavhareoff@gmail.com>
2022-12-13 16:26:52 +0530

    Added Models For hotel, city, flights. Created backend template

---
+++ zBackend/src/Config/MongoDB.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const DB_URL = process.env.MONGODB_URL
+
+
+const connect = async () => mongoose.connect(DB_URL);
+module.exports = connect; 
---
+++ zBackend/src/Features/Models/city.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const {states} = require('../../Utils/data.js')
+
+const CitySchema = new mongoose.Schema({
+    name:{type:String, required:true, unique:true},
+    state:{type:String, required:true, enum:states},
+    country:{type:String, enum:['India']},
+    location:{
+        lat:Number,
+        long:Number
+    },
+    places:[String]
+});
+
+const City = mongoose.model('city', CitySchema);
+
+module.exports = City;
