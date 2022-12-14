893f702e5527055613701547e8d2050cf2f115e9
itsAkash12 <akashviratsingh@gmail.com>
2022-12-15 01:09:12 +0530

    added:day 2 task

--- wonderlust/src/App.js
+++ wonderlust/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
+import { LandingPage } from "./Component/landingPage/LandingPage";
 import Navbar from './Component/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <LandingPage />
     </div>
   );
 }
