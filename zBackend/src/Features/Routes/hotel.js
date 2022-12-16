d13747f9bfb4782e56d45dd9bdd044f167c6e93d
Lv-23 <lokeshvyavhareoff@gmail.com>
2022-12-16 12:43:58 +0530

    added filter, sort, search api

--- zBackend/src/Features/Routes/hotel.js
+++ zBackend/src/Features/Routes/hotel.js
@@ -5,27 +5,132 @@ const app = express.Router();
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    const { page = 1, limit = 10, sort } = req.query;
+    let { page = 1, limit = 10, priceSort, name, stars, starsMin,
+        starsMax,rating, ratingMin, ratingMax,maxP, minP,
+        city, q, crTime, mTime } = req.query;
 
-    try {
-        if (sort) {
-            let k;
-            if (sort === 'asc') {
-                k = 1
-            } else if (sort === 'dsc') {
-                k = -1
-            }
-            const data = await Hotel.find().limit(limit).sort({ name: k }).skip((page - 1) * limit);
-            // console.log('data:', data)
+    if(!q){
+        q='';
+    }else{
+        q=q.toLowerCase();
+    }
+    if(!city){
+        city='';
+    }else{
+        city=city.toLowerCase();
+    }
+    
 
-            return res.send(data)
-        } else {
-            const data = await Hotel.find().limit(limit).skip((page - 1) * limit);
-            // console.log('data:', data)
-            return res.send(data)
+    try{
+        let data = await Hotel.find({"contactInfo.city":{$regex:city},name:{$regex:q}});
+
+        //Filtering
+
+        // stars
+        if(starsMin){            
+            data=data.filter((i)=>i.starRating>=+starsMin)
         }
-    } catch (err) {
-        res.send({ error: true, message: err.message })
+        if(starsMax){            
+            data=data.filter((i)=>i.starRating<=+starsMax)
+        }
+
+        // stars
+        if(starsMin){            
+            data=data.filter((i)=>i.rating>=+ratingMin)
+        }
+        if(starsMax){            
+            data=data.filter((i)=>i.rating<=+ratingMax)
+        }
+
+        // price
+        if(minP){            
+            data=data.filter((i)=>i.minPrice>=+minP)
+        }
+        if(maxP){            
+            data=data.filter((i)=>i.maxPrice<=+maxP)
+        }
+
+        //Sorting
+        // created
+        if(crTime){
+            if(crTime==='asc'){
+                data.sort((a, b)=>a.modifiedAt.getTime().getTime()-b.modifiedAt.getTime())
+            }else if(crTime==='desc'){
+                data.sort((a, b)=>b.modifiedAt.getTime()-a.modifiedAt.getTime())
+            }
+        }
+        // modified
+        if(mTime){
+            if(mTime==='asc'){
+                data.sort((a, b)=>a.modifiedAt.getTime().getTime()-b.modifiedAt.getTime())
+            }else if(mTime==='desc'){
+                data.sort((a, b)=>b.modifiedAt.getTime()-a.modifiedAt.getTime())
+            }
+        }
+        // name
+        if(name){
+            if(name==='asc'){
+                data.sort((a,b)=>{
+                    if(a.name>b.name){
+                        return -1
+                    }
+                    if(a.name<b.name){
+                        return 1
+                    }
+                    return 0
+                })
+            }else if(name==='desc'){
+                data.sort((a,b)=>{
+                    if(a.name>b.name){
+                        return 1
+                    }
+                    if(a.name<b.name){
+
+                        return -1
+                    }
+                    return 0
+                })
+            }
+        }
+        
+        // stars
+        if(stars){
+            if(stars==='asc'){
+                data.sort((a, b)=>a.starRating-b.starRating)
+            }else if(stars==='desc'){
+                data.sort((a, b)=>b.starRating-a.starRating)
+            }
+        }
+
+        // rating
+        if(rating){
+            if(rating==='asc'){
+                data.sort((a, b)=>a.rating-b.rating)
+            }else if(rating==='desc'){
+                data.sort((a, b)=>b.rating-a.rating)
+            }
+        }
+
+        // priceSort
+        if(priceSort){
+            if(priceSort==='asc'){
+                data.sort((a, b)=>a.minPrice-b.minPrice)
+            }else if(priceSort==='desc'){
+                data.sort((a, b)=>b.maxPrice-a.maxPrice)
+            }
+        }
+
+        // limit & page
+        data=data.filter((i, e)=>{
+            if(e>=(page-1)*limit && e<page*limit){
+                return true
+            }else{
+                return false
+            }
+        })
+        res.send(data);
+    }catch(err){
+        res.send({error:true, message:err.message})
     }
 })
 
