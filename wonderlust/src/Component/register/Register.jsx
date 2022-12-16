eda31452c4f5120c96b2b4d8f71ca43f313b2729
Lokesh777 <lokeshdevgan777@gmail.com>
2022-12-17 00:00:27 +0530

    layout form

--- wonderlust/src/Component/register/Register.jsx
+++ wonderlust/src/Component/register/Register.jsx
@@ -1,4 +1,4 @@
-import {VStack,Container,Box,Flex} from '@chakra-ui/react'
+import {VStack,Container,Box,Flex} from '@chakra-ui/react';
 import { useState } from 'react'
 import style from './styles.module.css'
 
@@ -7,30 +7,34 @@ const Register = () =>{
     const [active, setActive] = useState("/")
 
     return (
-        <VStack mt='5%' >
-    <Container maxW='md' >
-        <h1 className={style.h1}>Welcome WonderLust</h1>
-     <Box>
-        <img className={style.welcome_img} src="https://media.giphy.com/media/jF1oqkXJL0Mda/giphy.gif" alt="" />
-     </Box>
+        <VStack 
+        // mt='5%'
+        >
+           
+                   <Container maxW='md' >
+                        {/* <h1 className={style.h1}>Welcome WonderLust</h1> */}
+                            <Box>
+                                <img className={style.welcome_img} src="https://media.giphy.com/media/jF1oqkXJL0Mda/giphy.gif" alt="" />
+                            </Box>
 
-     <Flex className={style.flex}>
-        <Box className={style.box}>
-        <a href="/register"
-    className={active === "#register" ? "active" : ""}
-    
-    onClick={()=> setActive("/")}
-    >Signup</a>
-        </Box>
-        <Box className={style.box}>
-        <a href="/login_again"
-        className={active === "#login_again" ? "active" : ""}
-       onClick={()=> setActive("/")}
-       >Login</a>
-        </Box>
-       </Flex>
-    </Container>
-  </VStack>
+                        <Flex className={style.flex}>
+                            <Box className={style.box}>
+                            <a href="/register"
+                        className={active === "#register" ? "active" : ""}
+                        
+                        onClick={()=> setActive("/")}
+                        >Signup</a>
+                            </Box>
+                            <Box className={style.box}>
+                            <a href="/login_again"
+                            className={active === "#login_again" ? "active" : ""}
+                        onClick={()=> setActive("/")}
+                        >Login</a>
+                            </Box>
+                        </Flex>
+                   </Container>
+              
+       </VStack>
     )
 }
 export default Register;
